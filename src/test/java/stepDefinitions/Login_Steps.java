57f4e55b158d824ef48d9336feda030b59593327
Jonathan Linares <jonathanlinares91@gmail.com>
2022-08-05 11:14:06 -0400

    DriverFactory added

--- src/test/java/stepDefinitions/Login_Steps.java
+++ src/test/java/stepDefinitions/Login_Steps.java
@@ -1,35 +1,16 @@
 package stepDefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-
 import java.util.ArrayList;
+import static driver.DriverFactory.getDriver;
 
 public class Login_Steps {
-    private WebDriver driver;
-
-    @Before("@login")
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
-    }
-
-    @After("@login")
-    public void tearDown(){
-        driver.quit();
-    }
+    private WebDriver driver = getDriver();
 
     @Given("I access the webdriver university home page")
     public void i_access_the_webdriver_university_home_page() {
