03094a41e5177b8d7840a18c1a5e16083d84f465
Jonathan Linares <jonathanlinares91@gmail.com>
2022-08-07 21:46:17 -0400

    BasePage and methods implemented

--- src/test/java/stepDefinitions/Login_Steps.java
+++ src/test/java/stepDefinitions/Login_Steps.java
@@ -6,20 +6,21 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import java.util.ArrayList;
-import static driver.DriverFactory.getDriver;
+import pageObjects.BasePage;
 
-public class Login_Steps {
+import java.util.ArrayList;
+
+public class Login_Steps extends BasePage {
     private WebDriver driver = getDriver();
 
     @Given("I access the webdriver university home page")
     public void i_access_the_webdriver_university_home_page() {
-        driver.get("https://www.webdriveruniversity.com/");
+        navigateToURL("https://www.webdriveruniversity.com/");
     }
 
     @When("I click in login portal")
     public void i_click_in_login_portal() throws InterruptedException {
-        driver.findElement(By.xpath("//h1[contains(.,'LOGIN PORTAL')]")).click();
+        waitForWebElementAndClick(By.xpath("//h1[contains(.,'LOGIN PORTAL')]"));
         ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(newTab.get(1));
         Thread.sleep(2000);
@@ -27,17 +28,17 @@ public class Login_Steps {
 
     @When("I enter a username {}")
     public void i_enter_a_username(String username){
-        driver.findElement(By.xpath("//input[contains(@id,'text')]")).sendKeys(username);
+        sendKeys(By.xpath("//input[contains(@id,'text')]"),username);
     }
 
     @When("I enter a password {}")
     public void i_enter_a_password_webdriver123(String password) {
-        driver.findElement(By.xpath("//input[contains(@id,'password')]")).sendKeys(password);
+        sendKeys(By.xpath("//input[contains(@id,'password')]"),password);
     }
 
     @When("I click on login button")
     public void i_click_on_login_button() {
-        driver.findElement(By.xpath("//button[@id='login-button']")).click();
+        waitForWebElementAndClick(By.xpath("//button[@id='login-button']"));
     }
 
     @Then("I validate the successfully message")
